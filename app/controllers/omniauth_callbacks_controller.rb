0412ea841b05b3a2213292bef05da560f915031e
= <=>
2015-11-28 15:41:32 -0200

    28/11/15 fixing issues in the login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 class OmniauthCallbacksController < ApplicationController
 	def facebook
 		auth = request.env["omniauth.auth"]
-		raise auth.to_yaml
+		#raise auth.to_yaml
 		data = {
 			name: auth.info.first_name,
 			lastname: auth.info.last_name,
@@ -10,5 +10,17 @@ class OmniauthCallbacksController < ApplicationController
 			provider: auth.provider,
 			uid: auth.uid
 		}
+		@user = User.find_or_create_by_omniauth(data)
+		puts @user
+		puts "after printttttt"
+		if @user.persisted?
+			sign_in_and_redirect @user, event: :authentication
+		else
+			# If the email was already registered
+			# Twitter does not send email of the people
+			session[:omniauth_errors] = @user.errors.full_messages.to_sentence unless @user.save
+			session[:omniauth_data] = data
+			redirect_to new_user_registration_url
+		end
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,23 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   devise :omniauthable, omniauth_providers: [:facebook]
+
+
+  def self.find_or_create_by_omniauth(auth)
+  	user = User.where(provider: auth[:provider], uid: auth[:uid]).first
+    puts user
+    puts "self.find_or_create_by_omniauth in user.rb"
+  	unless user
+      puts "AQUI NO VA A ENTRAR COMPARE"
+  		user = User.create(
+  				name: auth[:name],
+  				last_name: auth[:lastname],
+  				username: auth[:username],
+  				email: auth[:email],
+  				uid: auth[:uid],
+  				provider: auth[:provider],
+  				password: Devise.friendly_token[0,20]
+  			)
+  	end
+  end
 end
