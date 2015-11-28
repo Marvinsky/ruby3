0bc086ceda06105726859ddde19e4de8f114afe3
= <=>
2015-11-28 17:07:40 -0200

    28/11/15 update new creation adding twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ActiveRecord::Base
 
   devise :omniauthable, omniauth_providers: [:facebook, :twitter]
 
+  validates :username, presence: true, uniqueness: true, 
+            length: {minimum: 5, maximum:20, too_short: "at leat 5 characteres", too_long: "Twitter does not accept more than 20 characteres."},
+            format: {with: /([A-Za-z0-9\-\_]+)/, message: " must only contain letters and numbers"}
 
   def self.find_or_create_by_omniauth(auth)
   	user = User.where(provider: auth[:provider], uid: auth[:uid]).first
